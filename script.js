aad68aaeebb605d44f6930b773442a13668d69a4
Brooks Jackson <brksjcksn03@protonmail.com>
2023-11-01 19:01:49 -0400

    Updating website

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Set the target date for the countdown to November 4, 2023, 3:00 PM EST
 const targetDate = new Date('2023-11-04T15:00:00-05:00').getTime(); // Note: -05:00 represents Eastern Standard Time (EST)
 
+// Get the countdown-display element
+const countdownDisplay = document.getElementById("countdown-display");
+
 // Update the countdown every second
 const interval = setInterval(function() {
     const currentDate = new Date().getTime();
@@ -8,16 +11,14 @@ const interval = setInterval(function() {
 
     if (timeLeft <= 0) {
         clearInterval(interval);
-        document.getElementById("countdown-display").innerHTML = "Countdown Expired";
+        countdownDisplay.innerText = "Countdown Expired";
     } else {
         const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-        document.getElementById("days").innerText = String(days).padStart(2, '0');
-        document.getElementById("hours").innerText = String(hours).padStart(2, '0');
-        document.getElementById("minutes").innerText = String(minutes).padStart(2, '0');
-        document.getElementById("seconds").innerText = String(seconds).padStart(2, '0');
+        const countdownString = `${String(days).padStart(2, '0')} Days : ${String(hours).padStart(2, '0')} Hours : ${String(minutes).padStart(2, '0')} Minutes : ${String(seconds).padStart(2, '0')} Seconds`;
+        countdownDisplay.innerText = countdownString;
     }
 }, 1000);
